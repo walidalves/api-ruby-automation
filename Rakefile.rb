8afb01022d1073933ea4e0121230b4b85b9017ea
Walid Arnous <walidarnous.1@gmail.com>
2023-04-07 15:36:41 -0300

    Inicio ajustes get

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,3 +5,8 @@ desc 'Execute Contract Tests'
 task :contract do
   sh 'bundle exec cucumber -t @contract'
 end
+
+desc 'Execute Get User Tests'
+task :get_users do
+  sh 'bundle exec cucumber -t @get_users'
+end
