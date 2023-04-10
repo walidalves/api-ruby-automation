86cc20c9e62962547510427ea97b611b2c324687
Walid Arnous <walidarnous.1@gmail.com>
2023-04-10 16:57:25 -0300

    get id user

--- features/steps/user_get_by_id_steps.rb
+++ features/steps/user_get_by_id_steps.rb
@@ -4,15 +4,16 @@ Dado ('que tenho uma massa configurada para o endpoint Usuarios.get para o cená
 end
 
 Quando ('enviar uma requisição para o endpoint Usuarios.get') do
-    @users = users.get_user("0uxuPY0cbmQhpEz1")
+    @id = "0uxuPY0cbmQhpEz1"
+    @users = users.get_user(@id)
 end
 
 Entao ('validar o retorno do endpoint Usuarios.get para o cenário {string}') do |type|
     if type.eql?('positivo')
-        expect(@users.response.code).to eql 200
-        expect(@users.response['message']).to be_an(String)
-        expect(@users.response['_id']).to be_an(String)
-      else
+        expect(@users.response.code.to_i).to eql(200)
+        expect(@users.message).not_to be_empty
+        expect(@users.body).not_to be_empty
+    else
         expect(@users.response.code).to eql 400
     end
 end
